46279fb98dd0b9d1109f47e4c943f11044146b6f
Nilay Raval <100033937+NILAYRAVAL@users.noreply.github.com>
2023-10-27 11:03:36 +0530

    Update App.js

--- App.js
+++ App.js
@@ -157,3 +157,95 @@ export default function App() {
     </>
   );
 }
+
+
+
+// export default function FruitPicker() {
+//   return (
+//     <label>
+//       Pick a fruit:
+//       <select name="selectedFruit" defaultValue="orange">
+//         <option value="apple">Apple</option>
+//         <option value="banana">Banana</option>
+//         <option value="orange">Orange</option>
+//       </select>
+//     </label>
+//   );
+// }
+
+
+               //  handle the multiple values with scroll 
+
+
+// export default function FruitPicker() {
+//   return (
+//     <label>
+//       Pick some fruits:
+//       <br/>
+//       <select
+//         name="selectedFruit"
+//         defaultValue={['orange', 'banana']}
+//         multiple={true}
+//       >
+//         <option value="apple">Apple</option>
+//         <option value="banana">Banana</option>
+//         <option value="orange">Orange</option>
+//         <option value="orange">Orange</option>
+//         <option value="orange">Orange</option>
+//         <option value="orange">Orange</option>
+//       </select>
+//     </label>
+//   );
+// }
+
+
+
+
+
+export default function EditPost() {
+  function handleSubmit(e) {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+    // You can pass formData as a fetch body directly:
+    fetch('/some-api', { method: form.method, body: formData });
+    // You can generate a URL out of it, as the browser does by default:
+    console.log(new URLSearchParams(formData).toString());
+    // You can work with it as a plain object.
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson); // (!) This doesn't include multiple select values
+    // Or you can get an array of name-value pairs.
+    console.log([...formData.entries()]);
+  }
+
+  return (
+    <form method="post" onSubmit={handleSubmit}>
+      <label>
+        Pick your favorite fruit: <br/>
+        <select name="selectedFruit" defaultValue="orange">
+          <option value="apple">Apple</option>
+          <option value="banana">Banana</option>
+          <option value="orange">Orange</option>
+        </select>
+      </label> <br/>
+      <label>
+        Pick all your favorite vegetables:
+        <br/>
+             <select
+          name="selectedVegetables"
+          multiple={true}
+          defaultValue={['corn', 'tomato']}
+        >
+          <option value="cucumber">Cucumber</option>
+          <option value="corn">Corn</option>
+          <option value="tomato">Tomato</option>
+        </select>
+      </label>
+      <hr />
+      <button type="reset">Reset</button>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
