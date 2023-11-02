65eb7defeb8ef4423b7efcf2d9fd7b5972ebf215
Nilay Raval <100033937+NILAYRAVAL@users.noreply.github.com>
2023-11-02 10:39:49 +0530

    Update Function.js

--- Function.js
+++ Function.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Calendar } from 'primereact/calendar';
+import { useState } from 'react';
 
 // function FunctionComponent ()  {
 //   return (
@@ -11,10 +13,21 @@ import React from 'react'
 
 
 let FunctionComponent = ({name}) => {
+    const [date, setDate] = useState(null);
     return (
        <>
         <h3>Hello {name}</h3>
-        <p>Let's Learn about Functional components</p>
+        {/* <p>Let's Learn about Functional components</p> */}
+       <button onClick={()=> alert ("Ruko muje 100+ karne do ")}>Click me!!</button>
+       <br/>
+      
+
+       <div className="card flex justify-content-center">
+            <Calendar value={date} onChange={(e) => setDate(e.value)} />
+        </div>
+        <br/>
+       
+        
        </>     
     );
 };
